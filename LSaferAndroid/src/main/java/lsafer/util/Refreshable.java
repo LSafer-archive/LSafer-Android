2b17d41342d3b3242a9b49b3418188c7aa6878e3
LSaferSE <lsaferse@gmail.com>
2019-07-30 07:41:25 +0300

    Create Refreshable.java

---
+++ LSaferAndroid/src/main/java/lsafer/util/Refreshable.java
@@ -0,0 +1,16 @@
+package lsafer.util;
+
+/**
+ * defines that the {@link android.view.View view} that implements this
+ * can be refreshed with it's data.
+ *
+ * @author LSaferSE
+ * @version 1 alpha (30-Jul-19)
+ * @since 30-Jul-19
+ */
+public interface Refreshable {
+    /**
+     * refresh view with the new data.
+     */
+    void refresh();
+}
